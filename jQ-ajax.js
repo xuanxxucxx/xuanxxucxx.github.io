da0e467667fea8b5cbd9a19d6607c1885cfa9817
xuanxxucxx <43163191+xuanxxucxx@users.noreply.github.com>
2019-03-19 13:47:55 +0800

    update js

--- jQ-ajax.js
+++ jQ-ajax.js
@@ -10,6 +10,10 @@ $(document).ready(function () {
 
         $.getJSON("favourites.json", function (result) {
             console.log(result);
+
+            $.each(result, function (index, value) {
+                console.log(index + "-" + value);
+            });
         });
 
     });
