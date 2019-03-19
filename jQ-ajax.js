8ea1e0fc25a6ddb025d53196e7a803c3899739f5
xuanxxucxx <43163191+xuanxxucxx@users.noreply.github.com>
2019-03-19 13:43:18 +0800

    update

--- jQ-ajax.js
+++ jQ-ajax.js
@@ -1,8 +1,16 @@
-$(document).ready(function(){
-    
-    $('#EpicButton').click(function(){
+$(document).ready(function () {
+
+    $('#EpicButton').click(function () {
 
         $("#AJAXDiv").load("textfile.txt");
+
+    });
+
+    $('#EpicButton1').click(function () {
+
+        $.getJSON("favourites.json", function (result) {
+            console.log(result);
+        });
 
     });
 
